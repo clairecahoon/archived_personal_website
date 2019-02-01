fe2befbb002760b6c9c5a4f7ae51e14c5d0cd1d1
Claire Cahoon <clairecahoon18@gmail.com>
2019-02-01 08:20:52 -0500

    Initial commit - uploading all files
    Uploading all of the files and images for my personal website

---
+++ assets/js/hover.zoom.conf.js
@@ -0,0 +1,36 @@
+        $(function() {
+        
+        	
+            $('.blue').hoverZoom({
+                overlayColor: '#005377',
+                zoom: 0
+            });
+            
+            $('.green').hoverZoom({
+                overlayColor: '#069954',
+                zoom: 0
+            });
+            
+            $('.black').hoverZoom({
+                overlayColor: '#2f2f2f',
+                zoom: 0
+            });
+            
+            $('.yellow').hoverZoom({
+                overlayColor: '#F2B307',
+                zoom: 0
+            });
+            
+            /* USAGE
+            
+            $('#pink').hoverZoom({
+                overlay: true, // false to turn off (default true)
+                overlayColor: '#2e9dbd', // overlay background color
+                overlayOpacity: 0.7, // overlay opacity
+                zoom: 25, // amount to zoom (px)
+                speed: 300 // speed of the hover
+            });
+            
+            */
+            
+        }); 
